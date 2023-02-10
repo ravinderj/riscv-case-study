988b79286db60703b914e8a46cd010825af0f1c4
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2023-02-10 14:50:46 +0530

    Implemented addition of vetors using openmp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "add.h"
+#include "omp.h"
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<int> a = {1, 2, 3, 4, 5, 6};
+  std::vector<int> b = {1, 2, 3, 4, 5, 6};
+#pragma omp parallel for
+  for (int i = 0; i < a.size(); i++) {
+    printf("sum of %d and %d =  %d\n", a[i], b[i], add(a[i], b[i]));
+    // printf("sum of %d and %d =  %d\n", 5, 6, add(5, 6));
+  }
+}
